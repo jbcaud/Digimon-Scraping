29adb8109ef4b7680da09ff56d80b9e18e0be3c2
jbcaud <jbcaud@gmail.com>
2022-08-02 23:40:11 -0400

    Reformatted and added todos

--- DigimonWebScraper.py
+++ DigimonWebScraper.py
@@ -8,25 +8,30 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
 import json
 
+# TODO: Possibly optimize this? Maybe using arguments[0] for everything
 def ValidationBS(driver, url):
     driver.get(url)
     print("Accepting cookies")
     driver.find_element(By.ID,'onetrust-accept-btn-handler').click()
+
     print("Choosing dropdowns")
     dropdown1 = Select(driver.find_element(By.ID, 'wpModal-language'))
     dropdown1.select_by_index(0)
-    dropdown1 = Select(driver.find_element(By.ID, 'wpModal-gemeTitle'))
-    dropdown1.select_by_index(1)
-    dropdown1 = Select(driver.find_element(By.ID, 'wpModal-country'))
-    dropdown1.select_by_index(0)
+    dropdown2 = Select(driver.find_element(By.ID, 'wpModal-gemeTitle'))
+    dropdown2.select_by_index(1)
+    dropdown3 = Select(driver.find_element(By.ID, 'wpModal-country'))
+    dropdown3.select_by_index(0)
+
     print("Accepting privacy policy")
     driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div/div/div[2]/div/div[4]/button').click()
     driver.execute_script("arguments[0].click();", driver.find_element(By.ID,'confirmPrivacyPolicy'))
     driver.execute_script("arguments[0].click();", driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div/div/div[3]/button'))
+
     print("Accepting TOS")
     driver.execute_script("arguments[0].click();", driver.find_element(By.ID,'confirmTos'))
     driver.execute_script("arguments[0].click();", driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div/div/div[3]/button'))
 
+# TODO: Separate into methods and iterate through cards.
 if __name__ == "__main__":
     num = "35750"
     url = "https://www.bandai-tcg-plus.com/card/" + num
@@ -35,6 +40,7 @@ if __name__ == "__main__":
     driver = webdriver.Firefox(options=fireFoxOptions)
     driver.implicitly_wait(10)
     ValidationBS(driver, url)
+
     print("Getting page")
     driver.get(url)
     html = driver.page_source
@@ -55,6 +61,7 @@ if __name__ == "__main__":
         else: key, val = key, val = b[0], b[1].strip()
         digimon[key] = val
     
+    # TODO: Rewrite this whole section. Similar to format above
     regulations = soup.find("dl", class_="detalistFlat").text
     footer = regulations.strip().split("\n")
     digimon["Legal Regulations"] = []
